19fd8ed244150013f1208c701e16225433bc15bb
Stéphane SOPPERA <stephane.soppera@example.com>
2021-06-02 22:14:43 +0200

    Basic extension.

---
+++ background.js
@@ -0,0 +1,24 @@
+// Chrome extension to control Deezer with a MIDI device.
+// Copyright (C) 2021  Stéphane SOPPERA
+//
+// This program is free software: you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see
+// <https://www.gnu.org/licenses/>.
+
+let color = '#3aa757';
+
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.storage.sync.set({ color });
+    console.log('Default background color set to %cgreen',
+                `color: ${color}`);
+});
