ee3f79e2c84175810ddf99419b949a1d31ab3f51
Stéphane SOPPERA <stephane.soppera@example.com>
2021-06-03 11:09:25 +0200

    Handle connection and disconnection.

--- patch_deezer.js
+++ patch_deezer.js
@@ -16,12 +16,73 @@
 // <https://www.gnu.org/licenses/>.
 console.log(`patching ${window.location} to add Deezer MIDI control`);
 
+function find_input(midi_access, input_name) {
+    for (let input of midi_access.inputs) {
+        if (input.name == input_name) {
+            return input;
+        }
+    }
+    return null;
+}
+
+class Connection {
+    constructor(midi_access) {
+        this.midi_access = midi_access;
+
+        midi_access.onstatechange = event => {
+            this.on_port_changed(event.port)
+                .catch(err => { console.error(err); });
+        }
+
+        this.connected = false;
+        this.connected_port_id = '';
+        this.connected_port_name = '';
+    }
+
+    async on_port_changed(port) {
+        console.log(`port changed: ${port.name} → ${port.state}`);
+        if (port.id === this.connected_port_id) {
+            console.assert(port.state === 'disconnected',
+                           port.state);
+            this.disconnect();
+        } else if (!this.connected) {
+            await this.connect();
+        } else {
+            console.assert(port.id !== this.connected_port_id,
+                           `port.id: ${port.id} port.state: ${port.state}`);
+        }
+    }
+
+    async connect() {
+        console.assert(!this.connected);
+        
+        let values = await get_local_storage('midi_input');
+        let input = find_input(this.midi_access, values.midi_input);
+        if (!input) {
+            console.warn(`Can't find the MIDI input named ${JSON.stringify(values.midi_input)}.`);
+            return;
+        }
+
+        console.log(`Connecting to ${JSON.stringify(input.name)}.`);
+        this.connected = true;
+        this.connected_port_id = input.id;
+        this.connected_port_name = input.name;
+    }
+
+    disconnect() {
+        console.log(`Disconnecting from ${JSON.stringify(this.connected_port_name)}.`);
+        this.connected = false;
+        this.connected_port_id = '';
+        this.connected_port_name = '';
+    }
+}
+
+let connection = null;
+
 navigator.requestMIDIAccess().then(async midi_access => {
     console.log(`got midi: ${midi_access}`);
     log_all_midi_inputs(midi_access);
 
-    let values = await get_local_storage('midi_input');
-    console.log(`midi_input: ${values.midi_input}`);
-
-    await set_local_storage({'midi_input': 4});
+    connection = new Connection(midi_access);
+    await connection.connect();
 }).catch(err => { console.error(err); })
